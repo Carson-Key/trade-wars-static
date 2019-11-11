d2d1b3fc4ff963fdeabdcd53482f354cc4dcf77d
Carson-Key <keyc@oregonstate.edu>
2019-11-11 13:33:34 -0800

    Updates

---
+++ precache-manifest.02de6646aaf1cb7c2b9f53e8a31dbf70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d23c2a018c1f1d889f57f348f01c4c",
+    "url": "/Carson-Key/trade-wars-static/index.html"
+  },
+  {
+    "revision": "ac51bc05a06a6604f773",
+    "url": "/Carson-Key/trade-wars-static/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f4ed790c9cc14706b670",
+    "url": "/Carson-Key/trade-wars-static/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac51bc05a06a6604f773",
+    "url": "/Carson-Key/trade-wars-static/static/js/2.e8df4a57.chunk.js"
+  },
+  {
+    "revision": "f4ed790c9cc14706b670",
+    "url": "/Carson-Key/trade-wars-static/static/js/main.82d0a67e.chunk.js"
+  },
+  {
+    "revision": "d42a24b1c44b857a157a",
+    "url": "/Carson-Key/trade-wars-static/static/js/runtime-main.ada557ef.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Carson-Key/trade-wars-static/static/media/logo.25bf045c.svg"
+  }
+]);
