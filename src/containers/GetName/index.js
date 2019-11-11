37465212515f8e97e98f0d1fdab4538b05404d3d
Colin Suckow <colin@suckow.dev>
2019-11-11 13:31:15 -0800

    Implement username input field

--- src/containers/GetName/index.js
+++ src/containers/GetName/index.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { InputGroup, InputGroupAddon, Button, Input } from "reactstrap";
+import "./styles.css";
 class GetName extends Component {
   render() {
     return (
-      <p>GetName</p>
-    )
+      <div class="get-name-background">
+        <div class="username-card">
+          <h1>Trade Wars!</h1>
+          <InputGroup>
+            <Input placeholder="Username" />
+            <InputGroupAddon addonType="append">
+              <Button>Play</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+      </div>
+    );
   }
 }
 
