6bc3eac57e6d61840d88fa00d19bf5ff60f635fa
Carson-Key <keyc@oregonstate.edu>
2019-11-25 21:35:04 -0800

    Broke my Git

--- src/js/actions/index.js
+++
@@ -1,5 +0,0 @@
-import { ADD_PLAYERNAME } from '../constants/action-types'
-
-export function addPlayername(payload) {
-  return { type: ADD_PLAYERNAME, payload }
-};
--- src/js/constants/action-types.js
+++
@@ -1,1 +0,0 @@
-export const ADD_PLAYERNAME = "ADD_PLAYERNAME"
